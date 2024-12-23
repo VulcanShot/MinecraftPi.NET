34c991a7e99fcd90bf936c35330d00907c8e0c32
VulcanShot <pieromarcelohp@gmail.com>
2024-12-22 22:08:50 -0500

    Added demo project

---
+++ source/MinecraftPiApi/MinecraftPiApi.Tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+using MinecraftPiApi;
+
+namespace MinecraftPiApi.Demo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Connection connection = new(IPAddress.Loopback, 1337);
+        connection.Send("Hello World");
+    }
+}
