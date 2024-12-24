c1bbdbedd6b4f3445f2bf7872e202c132b952c83
VulcanShot <pieromarcelohp@gmail.com>
2024-12-24 11:14:41 -0500

    Added simple demo

--- source/MinecraftPiApi/MinecraftPiApi.Tests/Program.cs
+++ source/MinecraftPiApi/MinecraftPiApi.Tests/Program.cs
@@ -1,14 +1,10 @@
-﻿using System.Net;
-
-using MinecraftPiApi;
-
-namespace MinecraftPiApi.Demo;
+﻿namespace MinecraftPiApi.Demo;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Connection connection = new(IPAddress.Loopback, 1337);
-        connection.Send("Hello World");
+        using var minecraft = Minecraft.Create();
+        minecraft.PostToChat("Hello, Minecraft!");
     }
 }
